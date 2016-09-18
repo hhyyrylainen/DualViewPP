585e3df9a9d36adae9bf579e0ab705d24b4153b5
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2016-09-18 22:51:32 +0300

    Added basic window with buttons

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 
 int main(int argc, char* argv[]){
 
-    
+    auto app =
+        Gtk::Application::create(argc, argv,
+            "org.gtkmm.examples.base");
+
+    Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file(
+        "../gui/main_gui.glade");
+
+    Gtk::Window* mainWindow = nullptr;
+    builder->get_widget("MainMenu", mainWindow);
+
+
+    return app->run(*mainWindow);    
 }
