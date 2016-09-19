91337229d2a957d09d3532c0726ad182b0aeb27b
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2016-09-19 10:48:30 +0300

    Added more layouts and now opens multiple windows

--- main.cpp
+++ main.cpp
@@ -9,9 +9,19 @@ int main(int argc, char* argv[]){
     Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file(
         "../gui/main_gui.glade");
 
+    app->register_application();
+    
+    Gtk::Window* WelcomeWindow = nullptr;
+    builder->get_widget("WelcomeWindow", WelcomeWindow);
+    
+    app->add_window(*WelcomeWindow);
+    WelcomeWindow->show();
+
     Gtk::Window* mainWindow = nullptr;
     builder->get_widget("MainMenu", mainWindow);
 
+    app->add_window(*mainWindow);
+    mainWindow->show();
 
-    return app->run(*mainWindow);    
+    return app->run();    
 }
