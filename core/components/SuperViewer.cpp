ff9307c06022c472de256d48468f7526480f8c8c
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2016-09-26 19:46:58 +0300

    Added bunch of events to SuperViewer

--- core/components/SuperViewer.cpp
+++ core/components/SuperViewer.cpp
@@ -7,19 +7,38 @@ using namespace DV;
 // ------------------------------------ //
 
 SuperViewer::SuperViewer(_GtkDrawingArea* area, Glib::RefPtr<Gtk::Builder> builder,
-    std::shared_ptr<Image> displayedResource) : Gtk::DrawingArea(area)
+    std::shared_ptr<Image> displayedResource) :
+    Gtk::DrawingArea(area), DisplayedResource(displayedResource)
 {
     // Add redraw timer //
     Glib::signal_timeout().connect(sigc::mem_fun(*this, &SuperViewer::_OnTimerCheck),
         CurrentTimer);
+
+    // Register for events //
+    add_events(
+        Gdk::POINTER_MOTION_MASK |
+        Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK |
+        Gdk::SCROLL_MASK |
+        Gdk::KEY_PRESS_MASK);
+
+    signal_motion_notify_event().connect(sigc::mem_fun(*this, &SuperViewer::_OnMouseMove));
+
+    signal_button_press_event().connect(
+        sigc::mem_fun(*this, &SuperViewer::_OnMouseButtonPressed));
+    signal_button_release_event().connect(
+        sigc::mem_fun(*this, &SuperViewer::_OnMouseButtonReleased));
     
+    signal_scroll_event().connect(sigc::mem_fun(*this, &SuperViewer::_OnScroll));
+    signal_key_press_event().connect(sigc::mem_fun(*this, &SuperViewer::_OnKeyPressed));
+
     // Do setup stuff //
-    DisplayImage = displayedResource->GetImage();
 }
 
 SuperViewer::~SuperViewer(){
 
-    LOG_WRITE("SUper destructed");
+    LOG_WRITE("Super destructed");
+
+    
 }
 // ------------------------------------ //
 bool SuperViewer::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
@@ -27,6 +46,19 @@ bool SuperViewer::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
     Gtk::Allocation allocation = get_allocation();
     const int width = allocation.get_width();
     const int height = allocation.get_height();
+
+    // Verify thumbnail mode //
+    const bool shouldBeThumbnailMode = ForceOnlyThumbnail || (width <= 256 && height <= 256);
+    if(shouldBeThumbnailMode != IsInThumbnailMode){
+
+        _SwitchToThumbnailMode(shouldBeThumbnailMode);
+    }
+
+    // If there is no image here, load the full image //
+    if(!DisplayImage){
+
+        _SetLoadedImage(DisplayedResource->GetImage());
+    }
 
     // paint the background
     //auto refStyleContext = get_style_context();
@@ -53,22 +85,46 @@ bool SuperViewer::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
         
     } else {
 
+        if(!IsImageReady){
+
+            // Reset things //
+            _OnNewImageReady();
+        }
+
         if(!DisplayImage->IsValid()){
 
             // Draw error message //
             
+            
         } else {
         
             // Draw positioned image //
-            auto img = DisplayImage->CreateGtkImage();
 
-            Gdk::Cairo::set_source_pixbuf(cr, img, 0, 0);
-            cr->rectangle(0, 0, img->get_width(), img->get_height());
-            cr->fill();
+            if(!CachedDrawnImage)
+                CachedDrawnImage = DisplayImage->CreateGtkImage();
+
+            _DrawCurrentImage(cr);
         }
     }
     
     return true;
+}
+
+void SuperViewer::_DrawCurrentImage(const Cairo::RefPtr<Cairo::Context>& cr) const{
+
+    LEVIATHAN_ASSERT(CachedDrawnImage, "CachedDrawnImage is invalid in draw current");
+
+    // Move to right position //
+    
+    
+    // Scale the whole drawing area //
+    cr->scale(ImageZoom, ImageZoom);
+
+    // Draw to a rectangle normally //
+    Gdk::Cairo::set_source_pixbuf(cr, CachedDrawnImage, 0, 0);
+    cr->rectangle(0, 0,
+        CachedDrawnImage->get_width(), CachedDrawnImage->get_height());
+    cr->fill();    
 }
 // ------------------------------------ //
 bool SuperViewer::IsImageReadyToShow() const{
@@ -76,7 +132,48 @@ bool SuperViewer::IsImageReadyToShow() const{
     if(!DisplayImage)
         return false;
 
+    // If image has been drawn already we can skip checks in DisplayImage
+    if(IsImageReady)
+        return true;
+
     return DisplayImage->IsLoaded();
+}
+
+void SuperViewer::_OnNewImageReady(){
+
+    LEVIATHAN_ASSERT(DisplayImage, "DisplayImage is invalid in _OnNewImageReady");
+
+    IsImageReady = true;
+
+    // Reset variables //
+    _ResetImageInstanceVariables();
+    
+    IsMultiFrame = DisplayImage->GetFrameCount() > 1;
+
+    LastFrame = ClockType::now();
+}
+
+void SuperViewer::_ResetImageInstanceVariables(){
+    
+    IsMultiFrame = false;
+    CurrentAnimationFrame = 0;
+    CachedDrawnImage.reset();
+}
+
+void SuperViewer::_SwitchToThumbnailMode(bool isthumbnail){
+
+    if(IsInThumbnailMode == isthumbnail)
+        return;
+
+    IsInThumbnailMode = isthumbnail;
+
+    _SetLoadedImage(DisplayedResource->GetThumbnail());
+}
+
+void SuperViewer::_SetLoadedImage(std::shared_ptr<LoadedImage> image){
+
+    DisplayImage = image;
+    IsImageReady = false;
 }
 // ------------------------------------ //
 bool SuperViewer::_OnTimerCheck(){
@@ -100,3 +197,61 @@ bool SuperViewer::_OnTimerCheck(){
     return true;
 }
 
+bool SuperViewer::_OnMouseMove(GdkEventMotion* motion_event){
+
+    if(DisableDragging || (!CanStartDrag && !DoingDrag))
+        return false;
+
+    LOG_WRITE("Drag");
+    queue_draw();
+
+    return true;
+}
+
+bool SuperViewer::_OnMouseButtonPressed(GdkEventButton* event){
+
+    if(DisableDragging)
+        return false;
+
+    // Double click
+    if(event->type == GDK_2BUTTON_PRESS)
+        LOG_WRITE("Double click");
+
+    // Left mouse //
+    if(event->button == 1)
+        CanStartDrag = true;
+
+    return false;
+}
+
+bool SuperViewer::_OnMouseButtonReleased(GdkEventButton* event){
+
+    if(DisableDragging)
+        return false;
+
+    // Left mouse //
+    if(event->button == 1)
+        CanStartDrag = false;
+
+    return false;
+}
+
+bool SuperViewer::_OnKeyPressed(GdkEventKey* event){
+
+    if(DisableKeyPresses)
+        return false;
+
+    LOG_WRITE("Key");
+
+    return false;
+}
+
+bool SuperViewer::_OnScroll(GdkEventScroll* event){
+
+    if(DisableMouseScroll)
+        return false;
+
+    LOG_WRITE("Scroll");
+    
+    return true;
+}
--- core/components/SuperViewer.h
+++ core/components/SuperViewer.h
@@ -5,12 +5,19 @@
 
 #include <gtkmm.h>
 
+#include <chrono>
+
 namespace DV{
 
 //! \brief Image viewing widget
 //!
 //! Manages drawing ImageMagick images with cairo
+//! \todo Make mouse disable function also disconnect singal handlers
+//! (https://developer.gnome.org/gtkmm-tutorial/stable/sec-disconnecting-signal-handlers.html.en)
 class SuperViewer : public Gtk::DrawingArea{
+
+    using ClockType = std::chrono::high_resolution_clock;
+    
 public:
 
     
@@ -25,20 +32,105 @@ protected:
 
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
 
+    //! \brief Draws the CachedDrawnImage with all the current settings
+    void _DrawCurrentImage(const Cairo::RefPtr<Cairo::Context>& cr) const;
+
     //! \brief Returns true if DisplayImage has finished loading
     bool IsImageReadyToShow() const;
+
+    //! \brief Called when switching from a full image to a thumbnail image
+    void _SwitchToThumbnailMode(bool isthumbnail);
+
+    //! \brief Resets variables both for _OnNewImageReady and _SwitchToThumbnailMode
+    void _ResetImageInstanceVariables();
 
     //! \brief Called from a timer, forces redraws when things happen
     //! \returns False when Gtk should disable the current timer. This happens
     //! when a new timer has been added
     bool _OnTimerCheck();
+
+    //! \brief Resets all per image variables to start rendering from a fresh state
+    void _OnNewImageReady();
+
+    //! \brief Sets the LoadedImage to be shown
+    void _SetLoadedImage(std::shared_ptr<LoadedImage> image);
+
+    bool _OnMouseMove(GdkEventMotion* motion_event);
+
+    bool _OnMouseButtonPressed(GdkEventButton* event);
+    bool _OnMouseButtonReleased(GdkEventButton* event);
+
+    bool _OnKeyPressed(GdkEventKey* event);
+
+    bool _OnScroll(GdkEventScroll* event);
     
 private:
 
+    //! The image to show
+    std::shared_ptr<Image> DisplayedResource;
+    
     //! Currently shown image resource
     std::shared_ptr<LoadedImage> DisplayImage;
 
+    //! Holds a cached version of DisplayImage. Prevents conversion happening on each frame
+    Glib::RefPtr<Gdk::Pixbuf> CachedDrawnImage;
+
+    //! Used to call _OnNewImageReady once
+    bool IsImageReady = false;
+
+    //! True when there are multiple frames in DisplayImage
+    bool IsMultiFrame = false;
+
+    float ImageZoom = 1.0f;
+
+    //Point BaseOffset = new Point(0, 0);
+
+    bool ResetZoom = true;
+
+    //! Set to true to allow this to react to image change keys / arrow keys
+    bool ReactToKeyPress = false;
+
+    bool OriginalIsAutoFit = true;
+    bool IsAutoFit = true;
+
+    //! If true currently has the thumbnail image loaded
+    bool IsInThumbnailMode = false;
+
+
+    //! Used to browse images in a collection
+    //std::shared_ptr<Collection> ContainedInCollection = nullptr;
+
+    //! Disables zooming with the scroll wheel
+    bool DisableMouseScroll = false;
+
+    //! Disables dragging with the mouse. Required to be false if drag events shouldn't
+    //! be handled by this
+    bool DisableDragging = false;
+
+    //! Disables moving to next/previous image with keys
+    bool DisableKeyPresses = false;
+
+    //! If true will only ever load a thumbnail of the image
+    bool ForceOnlyThumbnail = false;
+
+    //! Current frame of animation
+    size_t CurrentAnimationFrame = 0;
+
+    //! Used to show multi frame images at the right speed
+    ClockType::time_point LastFrame;
+
     //! Currently used timer for _OnTimerCheck in milliseconds
     int CurrentTimer = 1000;
+
+
+    //! True if mouse is down and dragging can start
+    bool CanStartDrag = false;
+    
+    //! True when dragging the image around
+    bool DoingDrag = false;
+
+    //        Point DragStartPos = new Point(0, 0);
+
+    // Point OffsetBeforeDrag = new Point(0, 0);
 };
 }
