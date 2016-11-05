f1b43adf1c48c41dbb8d45d0f513eb05958a136d
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2016-11-05 18:37:10 +0200

    Added script for stripping versions from .glade files

---
+++ StripGUIVersion.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+# Strips version requirements from glade files. Sample: <requires lib="gtk+" version="3.20"/>
+require 'nokogiri'
+
+Dir.foreach("gui") do |item|
+  next if item !~ /\.glade$/
+
+  puts "Stripping " + item
+
+  file = "gui/" + item
+  xml = Nokogiri::XML(File.read(file))
+
+  node = xml.at("interface/requires")
+
+  if node
+    node.remove
+  end
+  
+  File.write(file, xml.to_xml)
+  
+end
