28f4d925cf529c0fb261c30299c633ce8aaab5ec
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2019-04-26 12:33:25 +0300

    Added missing library for ubuntu install script and hopefully fixed
    leviathan wanting deps on first cmake run

--- InstallUbuntuDependencies.sh
+++ InstallUbuntuDependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs ubuntu packages required to compile DualView++
-sudo apt-get install cmake doxygen ruby ruby-dev tar libboost-dev libboost-filesystem-dev libboost-locale-dev libcrypto++-dev libcurl4-openssl-dev libgtk-3-dev libcanberra-gtk3-dev libgtkmm-3.0-dev libmagick++-dev libsqlite3-dev libhtmlcxx-dev libgumbo-dev
+sudo apt-get install cmake doxygen ruby ruby-dev tar libboost-dev libboost-filesystem-dev libboost-locale-dev libcrypto++-dev libcurl4-openssl-dev libgtk-3-dev libcanberra-gtk3-dev libgtkmm-3.0-dev libmagick++-dev libsqlite3-dev libhtmlcxx-dev libgumbo-dev libxmu-dev
 # Make a link to tar
 sudo ln -s /bin/tar /usr/bin/tar
 # Submodule packages
