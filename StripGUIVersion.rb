ea492f0411f99c5370b21538fcbee90f9db66928
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-02-07 11:19:51 +0200

    Added rubocop config, probably fixed strip GUI version script

--- StripGUIVersion.rb
+++ StripGUIVersion.rb
@@ -1,21 +1,20 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 # Strips version requirements from glade files. Sample: <requires lib="gtk+" version="3.20"/>
 require 'nokogiri'
 
-Dir.foreach("gui") do |item|
+Dir.foreach('resources/gui') do |item|
   next if item !~ /\.glade$/
 
-  puts "Stripping " + item
+  puts 'Stripping ' + item
 
-  file = "gui/" + item
+  file = 'gui/' + item
   xml = Nokogiri::XML(File.read(file))
 
-  node = xml.at("interface/requires")
+  node = xml.at('interface/requires')
 
-  if node
-    node.remove
-  end
-  
+  node&.remove
+
   File.write(file, xml.to_xml)
-  
 end
