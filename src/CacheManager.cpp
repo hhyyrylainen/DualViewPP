942a1280421f32101d0504eef8f9c2f316cdf7bd
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2021-02-09 10:50:48 +0200

    Added unload if cache has a ton of stuff in it

--- src/CacheManager.cpp
+++ src/CacheManager.cpp
@@ -260,6 +260,27 @@ void CacheManager::_RunCacheCleanupThread()
                     ++iter;
                 }
             }
+
+            // Force unload if too many
+            int unloadsRemaining = 40;
+
+            while(ImageCache.size() > DUALVIEW_SETTINGS_MAX_CACHED_IMAGES && unloadsRemaining > 0){
+                // TODO: not the optimal algorithm but oh well, this is run in the background
+                auto oldest = ImageCache.begin();
+                auto oldestAge = time - (*oldest)->GetLastUsed();
+
+                for(auto iter = ImageCache.begin() + 1; iter != ImageCache.end(); ++iter){
+                    const auto age = (time - (*iter)->GetLastUsed());
+
+                    if(age > oldestAge) {
+                        oldest = iter;
+                        oldestAge = age;
+                    }
+                }
+
+                ImageCache.erase(oldest);
+                --unloadsRemaining;
+            }
         }
     }
 }
