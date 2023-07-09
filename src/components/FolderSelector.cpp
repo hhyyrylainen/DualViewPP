50cac36bc1b355dde60e83dc4503197e1bc85766
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2023-07-09 13:08:57 +0300

    Folder selectors can now be made to remember the target folder

--- src/components/FolderSelector.cpp
+++ src/components/FolderSelector.cpp
@@ -1,23 +1,24 @@
 // ------------------------------------ //
 #include "FolderSelector.h"
 
-#include "FolderListItem.h"
 #include "resources/Folder.h"
 
 #include "Database.h"
 #include "DualView.h"
+#include "FolderListItem.h"
 
 using namespace DV;
-// ------------------------------------ //
 
+// ------------------------------------ //
+bool FolderSelector::RememberingGlobalPath = false;
+VirtualPath FolderSelector::RememberedGlobalPath = VirtualPath();
 
 FolderSelector::FolderSelector() : Gtk::Box(Gtk::ORIENTATION_VERTICAL)
 {
     _CommonCtor();
 }
 
-FolderSelector::FolderSelector(_GtkBox* widget, Glib::RefPtr<Gtk::Builder> builder) :
-    Gtk::Box(widget)
+FolderSelector::FolderSelector(_GtkBox* widget, Glib::RefPtr<Gtk::Builder> builder) : Gtk::Box(widget)
 {
     _CommonCtor();
 }
@@ -47,27 +48,48 @@ void FolderSelector::_CommonCtor()
     set_hexpand(true);
     set_vexpand(true);
 
+    TargetLocationLocked.set_label("Remember target location");
+    TargetLocationLocked.set_tooltip_text(
+        "When checked the folder selectors will remember the currently selected location when opening or when "
+        "resetting the location, useful to quickly import many things to the same folder");
+
+    add(TargetLocationLocked);
+
     // Attach events //
-    CreateNewFolder.signal_clicked().connect(
-        sigc::mem_fun(*this, &FolderSelector::_CreateNewFolder));
+    CreateNewFolder.signal_clicked().connect(sigc::mem_fun(*this, &FolderSelector::_CreateNewFolder));
 
     show_all_children();
 
     RegisterNavigator(PathEntry, UpFolder);
 
-    // Show the root folder
-    GoToRoot();
+    // Remember the initial path if checked
+    if (HasLockedInGlobalTargetPath())
+    {
+        TargetLocationLocked.set_active();
+        ControllingGlobalTarget = true;
+
+        GoToPath(GetLockedInGlobalTargetPath());
+    }
+    else
+    {
+        // Show the root folder
+        GoToRoot();
+    }
+
+    TargetLocationLocked.signal_toggled().connect(sigc::mem_fun(*this, &FolderSelector::OnLockTargetModeChanged));
 }
 
-FolderSelector::~FolderSelector() {}
 // ------------------------------------ //
 void FolderSelector::OnFolderChanged()
 {
-    if(!CurrentFolder) {
-
+    if (!CurrentFolder)
+    {
         LOG_ERROR("SelectedFolder is null in FolderSelector");
         return;
     }
+
+    if (ControllingGlobalTarget)
+        SetLockedInGlobalTargetPath(CurrentPath);
 
     // TODO: move this load to a background thread
     std::vector<std::shared_ptr<Folder>> folders =
@@ -75,36 +97,80 @@ void FolderSelector::OnFolderChanged()
 
     auto changefolder = std::make_shared<ItemSelectable>();
 
-    changefolder->AddFolderSelect([this](ListItem& item) {
-        FolderListItem* asfolder = dynamic_cast<FolderListItem*>(&item);
+    changefolder->AddFolderSelect(
+        [this](ListItem& item)
+        {
+            auto* asFolder = dynamic_cast<FolderListItem*>(&item);
 
-        if(!asfolder)
-            return;
+            if (!asFolder)
+                return;
 
-        MoveToSubfolder(asfolder->GetFolder()->GetName());
-    });
+            MoveToSubfolder(asFolder->GetFolder()->GetName());
+        });
 
-    FolderContents.SetShownItems(folders.begin(), folders.end(), changefolder,
-        SuperContainer::POSITION_KEEP_MODE::SCROLL_TO_TOP);
+    FolderContents.SetShownItems(
+        folders.begin(), folders.end(), changefolder, SuperContainer::POSITION_KEEP_MODE::SCROLL_TO_TOP);
 
     PathEntry.set_text(CurrentPath.GetPathString());
 }
+
 // ------------------------------------ //
 void FolderSelector::_CreateNewFolder()
 {
     // If the user has typed something in the entry use that as the name for the new folder //
-    std::string userinput = PathEntry.get_text();
+    std::string userInput = PathEntry.get_text();
 
-    userinput.erase(0, CurrentPath.GetPathString().length());
-    // userinput is most likely empty here //
+    userInput.erase(0, CurrentPath.GetPathString().length());
+    // userInput is most likely empty here //
 
-
-    Gtk::Window* parent = dynamic_cast<Gtk::Window*>(this->get_toplevel());
+    auto* parent = dynamic_cast<Gtk::Window*>(this->get_toplevel());
     LEVIATHAN_ASSERT(parent, "FolderSelector has no Parent Gtk::Window");
 
-    DualView::Get().RunFolderCreatorAsDialog(CurrentPath, userinput, *parent);
+    DualView::Get().RunFolderCreatorAsDialog(CurrentPath, userInput, *parent);
 
     // Update folders
     OnFolderChanged();
 }
+
 // ------------------------------------ //
+void FolderSelector::OnLockTargetModeChanged()
+{
+    if (TargetLocationLocked.get_active())
+    {
+        ControllingGlobalTarget = true;
+        SetLockedInGlobalTargetPath(CurrentPath);
+    }
+    else
+    {
+        if (ControllingGlobalTarget)
+        {
+            ControllingGlobalTarget = false;
+            ClearLockedInGlobalTargetPath();
+        }
+    }
+}
+
+// ------------------------------------ //
+bool FolderSelector::HasLockedInGlobalTargetPath()
+{
+    return RememberingGlobalPath;
+}
+
+VirtualPath FolderSelector::GetLockedInGlobalTargetPath()
+{
+    if (!RememberingGlobalPath)
+        return {};
+
+    return RememberedGlobalPath;
+}
+
+void FolderSelector::SetLockedInGlobalTargetPath(const VirtualPath& path)
+{
+    RememberingGlobalPath = true;
+    RememberedGlobalPath = path;
+}
+
+void FolderSelector::ClearLockedInGlobalTargetPath()
+{
+    RememberingGlobalPath = false;
+}
--- src/components/FolderSelector.h
+++ src/components/FolderSelector.h
@@ -1,25 +1,33 @@
 #pragma once
 
-#include "FolderNavigatorHelper.h"
-#include "SuperContainer.h"
+#include <memory>
 
 #include <gtkmm.h>
 
-#include <memory>
+#include "FolderNavigatorHelper.h"
+#include "SuperContainer.h"
 
-namespace DV {
+namespace DV
+{
 
 class Folder;
 
 //! \brief Allows selecting a DV::Folder
-class FolderSelector : public Gtk::Box, public FolderNavigatorHelper {
+class FolderSelector : public Gtk::Box,
+                       public FolderNavigatorHelper
+{
 public:
     //! \brief Non-glade constructor
     FolderSelector();
 
     //! \brief Constructor called by glade builder when loading a widget of this type
     FolderSelector(_GtkBox* widget, Glib::RefPtr<Gtk::Builder> builder);
-    ~FolderSelector();
+    ~FolderSelector() override = default;
+
+    inline bool TargetPathLockedIn() const
+    {
+        return ControllingGlobalTarget;
+    }
 
     //! \brief Returns the current folder. This is never null
     std::shared_ptr<Folder> GetFolder() const
@@ -36,12 +44,19 @@ public:
 private:
     void _CommonCtor();
 
+    static bool HasLockedInGlobalTargetPath();
+    static VirtualPath GetLockedInGlobalTargetPath();
+    static void SetLockedInGlobalTargetPath(const VirtualPath& path);
+    static void ClearLockedInGlobalTargetPath();
+
 protected:
     //! \brief Reads the properties of the SelectedFolder
     void OnFolderChanged() override;
 
     // Gtk callbacks //
     void _CreateNewFolder();
+
+    void OnLockTargetModeChanged();
 
 protected:
     //! Shows the CurrentPath to the user and can be used to paste in a path
@@ -54,6 +69,14 @@ protected:
 
     //! Contents of the current folder
     SuperContainer FolderContents;
+
+    Gtk::CheckButton TargetLocationLocked;
+
+    bool ControllingGlobalTarget = false;
+
+private:
+    static bool RememberingGlobalPath;
+    static VirtualPath RememberedGlobalPath;
 };
 
 } // namespace DV
